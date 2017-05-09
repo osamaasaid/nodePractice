916d57deb23f23659c1e4f6ed5de59507b466822
osamaasaid <osamaasaid@gmail.com>
2017-05-08 17:20:56 -0700

    created basic express app

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-var faker = require("faker");
-// var fakeProduct = faker.commerce.productName();
-// var fakePrice = faker.commerce.price();
-console.log("Our inventory:");
- for(var i = 0; i <= 10; i++){
-   console.log(faker.commerce.productName() + " - $" + faker.commerce.price());
- }
+console.log("Express app!!!");
+var express = require("express");
+var app = express();
+
+// "/" => "Hi"
+// "/bye" => "Goodbye"
+// "/cat" => "meaw"
+
+app.get("/", function(req, res){
+  res.send("Hi there!");
+});
+
+
+app.listen(3000, function() {
+  console.log('app is running on port 3000');
+});
